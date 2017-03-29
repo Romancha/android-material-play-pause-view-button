39597585c2dd88b048806d5096119301ef6d1f8b
Romancha <Romanchabest55@gmail.com>
2017-03-29 17:46:11 +0700

    initialize commit

---
+++ app/src/main/java/com/romancha/playpauseview/PlayPauseTestActivity.java
@@ -0,0 +1,24 @@
+package com.romancha.playpauseview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.romancha.playpause.PlayPauseView;
+
+
+public class PlayPauseTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_pause_test);
+        final PlayPauseView view = (PlayPauseView) findViewById(R.id.play_pause_view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.toggle();
+            }
+        });
+    }
+}
